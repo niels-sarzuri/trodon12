cf045bacbd526e829259e77b7512338e5984e663
niels-sarzuri <103546213+niels-sarzuri@users.noreply.github.com>
2022-10-01 15:55:48 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function login(){
+    let usuario = document.getElementById('user').value
+    let password = document.getElementById('password').value
+
+    let user ="niels"
+    let pass='12345'
+
+     if(usuario==user && password==pass){
+        alert('BIENVENIDOS')
+        location.href='index.html'
+     }else{
+        alert('usuario o contrasena incorrecta')
+        location.href='login.html'
+     }
+}
+function carrito(){
+ location.href ="carrito.html"
+}
